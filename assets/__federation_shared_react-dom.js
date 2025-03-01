857e654c4f74b23e8a6b0411062a80c4612cf1ee
akirayorunoe <akirayorunoe@gmail.com>
2025-03-01 19:30:00 +0700

    Updates

--- assets/__federation_shared_react-dom.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./_commonjsHelpers.js";import{r as o}from"./index3.js";var t=o();const m=r(t);export{m as default};
--- assets/__federation_shared_react.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./_commonjsHelpers.js";import{r as t}from"./index2.js";var e=t();const s=r(e);export{s as default};
